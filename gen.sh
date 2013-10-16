09ef9ee8118fbb827530b52459008cecd7cadadd
Mayank Sharma <mayanks@gmail.com>
2013-10-16 10:34:58 +0530

    single css and js

---
+++ gen.sh
@@ -0,0 +1,2 @@
+cat css/bootstrap/bootstrap.min.css css/bootstrap/bootstrap-responsive.min.css css/style.css css/media-queries.css > css/main.css
+cat js/jquery.1.8.2.js js/nav/jquery.scrollTo.js js/nav/jquery.nav.js js/retina/retina.js js/fancybox/jquery.fancybox.pack.js js/jquery.fitvids.min.js js/jquery.placeholder.min.js js/jquery-func.js > js/main.js
